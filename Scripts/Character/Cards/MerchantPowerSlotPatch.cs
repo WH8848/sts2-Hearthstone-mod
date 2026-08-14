b0584336b7466b8183cbdfc0b8e918ad6092eafe
WH8848 <1764912246@qq.com>
2026-08-14 12:09:59 +0800

    修复商店能力槽补丁缺少 Player using（编译错误）

--- Scripts/Character/Cards/MerchantPowerSlotPatch.cs
+++ Scripts/Character/Cards/MerchantPowerSlotPatch.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Factories;
 using MegaCrit.Sts2.Core.Models;
 
