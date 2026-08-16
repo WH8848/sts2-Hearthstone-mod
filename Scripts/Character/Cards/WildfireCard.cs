1534b976e39d5162481f259b522f0d970da60134
WH8848 <1764912246@qq.com>
2026-08-16 22:40:19 +0800

    野火升级预览显示数值递增
    - CanonicalVars 加 DamageVar(1) 并显示为 {Damage:diff()}：
      升级预览时数值 1→2→3… 绿色高亮
    - OnUpgrade 用 UpgradeValueBy(1)（无限升级每次 +1）；
      OnPlay 按 DynamicVars 当前值施加加成

--- Scripts/Character/Cards/WildfireCard.cs
+++ Scripts/Character/Cards/WildfireCard.cs
@@ -5,6 +5,7 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
 using jaina.Scripts.Character.Powers;
 using STS2RitsuLib.Interop.AutoRegistration;
 using STS2RitsuLib.Scaffolding.Content;
