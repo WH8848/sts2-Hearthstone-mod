1106317fee9b2a09b4bb8fa12b3dc0a132af6c70
WH8848 <1764912246@qq.com>
2026-08-16 21:36:30 +0800

    新增：鲁莽的学徒（1费罕见随从，3/5）
    - 战吼：向随机敌人发射8次你的英雄技能
      （每次免费自动打出当前英雄技能：默认火焰冲击或英雄卡替换后的技能，
      随机合法敌人目标，发射出的技能卡消耗）
    - wiki.gg 原画 cover 250x190；注册随从映射 + zhs/eng 本地化

--- Scripts/Character/Minions/RecklessApprenticeMinion.cs
+++ Scripts/Character/Minions/RecklessApprenticeMinion.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
