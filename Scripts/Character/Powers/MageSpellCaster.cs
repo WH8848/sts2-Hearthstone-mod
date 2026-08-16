fcdfcb9afcc0006a6c9666d371c9be80306d2a45
WH8848 <1764912246@qq.com>
2026-08-16 20:50:00 +0800

    魔法智慧之球固定六张法术；终极索兰莉安含升级形态
    - MageSpellCaster 拆分两个施放入口：
      CastRandomMageSpell（魔法智慧之球固定六张，不展开升级形态）
      CastRandomMageSpellFromAll（终极索兰莉安全池含升级形态）
    - 终极索兰莉安战吼改用全池入口（未升级+升级形态都可能被施放）

--- Scripts/Character/Powers/MageSpellCaster.cs
+++ Scripts/Character/Powers/MageSpellCaster.cs
@@ -7,6 +7,7 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
 using jaina.Scripts.Character.Cards;
 using MinionLib.Targeting;
 
