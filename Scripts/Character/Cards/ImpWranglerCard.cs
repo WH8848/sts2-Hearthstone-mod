5748d66a3b61bc8a673ab113a71b98c01b344cd6
WH8848 <1764912246@qq.com>
2026-08-16 23:59:04 +0800

    小精灵驾驭者悬停提示：显示灌注额外召唤的小精灵衍生物卡（参考灵体采集者）

--- Scripts/Character/Cards/ImpWranglerCard.cs
+++ Scripts/Character/Cards/ImpWranglerCard.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.HoverTips;
 using jaina.Scripts.Character.Minions;
 using STS2RitsuLib.Interop.AutoRegistration;
 
@@ -8,7 +9,7 @@ namespace jaina.Scripts.Character.Cards;
 
 /// <summary>
 /// 小精灵驾驭者 (Imp Wrangler) - 2费随从卡（普通）。
-/// 战吼：灌注并触发你的英雄技能。属性 4/4。
+/// 战吼：灌注并触发你的英雄技能（灌注会额外召唤 1/1 小精灵）。属性 4/4。
 /// </summary>
 [RegisterCard(typeof(JainaCardPool))]
 public sealed class ImpWranglerCard : JainaMinionCardTemplate
@@ -17,6 +18,17 @@ public sealed class ImpWranglerCard : JainaMinionCardTemplate
     /// 卡牌原画：程序绘制的"小精灵驾驭者"占位图（无炉石原卡）
     /// </summary>
     public override string CustomPortraitPath => "res://assets/card_art/imp_wrangler.png";
+
+    /// <summary>
+    /// 悬停提示：显示灌注额外召唤的小精灵衍生物卡（参考灵体采集者）
+    /// </summary>
+    protected override IEnumerable<IHoverTip> AdditionalHoverTips
+    {
+        get
+        {
+            yield return new CardHoverTip(MegaCrit.Sts2.Core.Models.ModelDb.Card<ImpCard>());
+        }
+    }
 
     protected override Type MinionType => typeof(ImpWranglerMinion);
 
