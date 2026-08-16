3b5bfac698d87ad3db6916f65c5bec5dcf1bcc78
WH8848 <1764912246@qq.com>
2026-08-16 22:12:21 +0800

    新增：寒冰屏障（1费稀有能力，冰霜，不可升级）
    - 当你将要承受致命伤害时，防止这些伤害，并在本回合中免疫：
      IceBlockPower 挂在玩家身上（ModifyDamageMultiplicative：
      致命伤害归零并进入免疫，免疫期间所有伤害无效），回合结束移除
    - wiki.gg Ice Block 原画 cover 250x190；程序绘制冰盾图标；
      zhs/eng 本地化（卡+光环）

--- Scripts/Character/Powers/IceBlockPower.cs
+++ Scripts/Character/Powers/IceBlockPower.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
