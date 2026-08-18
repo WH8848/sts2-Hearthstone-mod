05eab499a26711c785c9cb8c53b2e6234297e1c4
WH8848 <1764912246@qq.com>
2026-08-18 21:35:01 +0800

    吉安娜卡框颜色改为 #69CCF0（RGB 105,204,240 冰蓝）

--- Scripts/Character/JainaCardPool.cs
+++ Scripts/Character/JainaCardPool.cs
@@ -22,8 +22,8 @@ public class JainaCardPool : TypeListCardPoolModel, IModColorfulPhilosophersCard
 
     public override string? BigEnergyIconPath => "res://assets/energy/energy_jaina_big.png";
 
-    // 使用原版储君的能量图标颜色作为参考（冰蓝色调）
-    public override Color DeckEntryCardColor => new(0.4f, 0.7f, 1f);
+    // 吉安娜主题卡框颜色：#69CCF0（RGB 105, 204, 240，冰蓝）
+    public override Color DeckEntryCardColor => new("#69CCF0");
 
     public override bool IsColorless => false;
 
